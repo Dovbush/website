7d87138dd5801de72d042b5184430480401568a1
dovbush.it@gmail.com <dovbush.it@gmail.com>
2018-10-08 00:31:13 +0300

    Project commit

--- 4front (wordpress)/www5.4front.se/wp-content/themes/4front/single.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $post = $wp_query->post;
- 
-  if ((in_category('vacancies'))||(in_category('jobb'))) { 
-      include(TEMPLATEPATH.'/single-vacancies.php');
-  } else {
-      include(TEMPLATEPATH.'/single-news.php');
-  }
-?>
